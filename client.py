63385cada950803b0ead9c263fb1e9579246ecd7
nickshorter <nickshorter888@gmail.com>
2021-01-10 08:50:16 -0600

    Playing around with sockets

--- client.py
+++ client.py
@@ -1,18 +1,19 @@
 import socket
 import pickle
-
+import time
 class hello_server():
     def __init__(self) -> None:
         super().__init__()
     def hello(self):
         return "hello From stuff"
+while True:
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.connect((socket.gethostname(), 29888))
+    hello = bytes("hello from client 1", "utf-8")
+    server_socket.sendall(hello)
+    data = server_socket.recv(1024)
+    print(repr(data))
+    time.sleep(1)
+    continue
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.connect((socket.gethostname(), 29888))
-hello = hello_server()
-
-server_socket.sendall(pickle.dumps(hello))
-data = server_socket.recv(1024)
-
-print(repr(data))
 server_socket.close()
--- server.py
+++ server.py
@@ -9,14 +9,14 @@ class DrinkServer():
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind((socket.gethostname(), 29888))
         sock.listen(5)
+        print("Server listening on port 29888...")
         while True:
             (con, address) = sock.accept()
             data = con.recv(1024)
             
             con.send(b"Connected to server")
-            con.close()
-            stuff = pickle.loads(data)
-            print(stuff)
+            
+            print(data)
 
 if __name__ == "__main__":
     alfred = DrinkServer()
