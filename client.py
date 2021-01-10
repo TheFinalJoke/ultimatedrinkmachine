be873825a297a708e16424adf5219bfbf499491f
Nick Shorter <36796370+TheFinalJoke@users.noreply.github.com>
2021-01-10 14:20:19 -0600

    Add files via upload

---
+++ client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3 
+
+import socket
+import pickle
+import time
+from lib.base import BaseDrinkClass
+
+class hello_server():
+    def __init__(self) -> None:
+        super().__init__()
+    def hello(self):
+        return "hello From stuff"
+
+while True:
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.connect((socket.gethostname(), 29888))
+    hello = bytes("hello from client 1", "utf-8")
+    server_socket.sendall(hello)
+    data = server_socket.recv(1024)
+    print(repr(data))
+    time.sleep(1)
+    continue
+
+server_socket.close()
