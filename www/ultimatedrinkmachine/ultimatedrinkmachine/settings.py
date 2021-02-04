dd26eb9ee3bd0db302d3abdaadae95ccb72577e9
Nick Shorter <nickshorter888@gmail.com>
2021-02-03 20:33:05 -0600

    drink server

--- www/ultimatedrinkmachine/ultimatedrinkmachine/settings.py
+++ www/ultimatedrinkmachine/ultimatedrinkmachine/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*i&ofbi4irz6l%x^_)gb99oy$zo-4m8^s@@t8@b6kz4vo^q$4='
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
